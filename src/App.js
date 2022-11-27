cf2a1993c42535808f71e15bd88d771a467a685f
Sergey <myemail@example.com>
2022-11-27 20:13:24 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Cart from "./components/Cart";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Cart
+        title={"Card title"}
+        text={
+          " Some quick example text to build on the card title and make up the\n" +
+          "          bulk of the card's content."
+        }
+      >
+        <img
+          alt={""}
+          src={"https://img.freepik.com/premium-photo/3d-illustration-of-a-glass-sphere-with-many-faces-crystals-scatter-on-a-black-background-cyber-ball-sphere_116124-6402.jpg?w=1380"}
+          className="card-img-top"
+        />
+      </Cart>
+      <Cart
+        title={"Special title treatment"}
+        text={
+          "With supporting text below as a natural lead-in to additional content."
+        }
+      />
     </div>
   );
 }
